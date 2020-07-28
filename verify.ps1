9366ffbe4dddadbdd5a48d5fc22add5be672b736
escape0707 <tothesong@gmail.com>
2020-07-28 21:09:31 +0800

    Update verify.ps1

--- verify.ps1
+++ verify.ps1
@@ -4,8 +4,8 @@ g++ ac.cpp -o ac.exe
 g++ $problem.cpp -o $problem.exe
 while ($true) {
   gen.exe
-  ac.exe < input.txt > answer.txt
-  $problem.exe < input.txt > output.txt
+  cat input.txt | ac.exe > answer.txt
+  cat input.txt | $problem.exe > output.txt
   if (diff (cat file1) (cat file2)) {
     break
   }

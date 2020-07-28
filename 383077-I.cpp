4cc808cb7ef9681af819d2039d657b1c0e2692f9
escape0707 <tothesong@gmail.com>
2020-07-28 13:49:48 +0800

    Update 383077-I to final version

--- 383077-I.cpp
+++ 383077-I.cpp
@@ -8,7 +8,7 @@ static constexpr int MAX_CNT = 10;
 static array<array<bool, MAX_CNT>, MAX_CNT> is_friend;
 static int kid_cnt, ball_cnt;
 
-static bool check_friendship(int kid_id, const vector<int> &group) {
+static bool check_friendship(const int kid_id, const vector<int> &group) {
   return all_of(begin(group), end(group), [&kid_id](const int kid_in_group_id) {
     return is_friend[kid_id][kid_in_group_id];
   });
@@ -48,15 +48,17 @@ int main() {
     for (int kid_id = 0; kid_id < kid_cnt; ++kid_id) {
       int friend_cnt;
       cin >> friend_cnt;
-      for (int friend_no = 0; friend_no < friend_cnt; ++friend_no) {
+      while (friend_cnt--) {
         int friend_id;
         cin >> friend_id;
         is_friend[kid_id][friend_id] = true;
-        // is_friend[friend_id][kid_id] = true;
       }
     }
 
-    group_collection = vector<vector<int>>(ball_cnt);
+    group_collection.resize(ball_cnt);
+    for (auto &group : group_collection) {
+      group.resize(0);
+    }
     current_group_count = 0;
     kid_id = 0;
     cout << (dfs() ? "YES" : "NO") << endl;

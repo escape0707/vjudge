3a9eafeefb0c9cac9f4d2ea7be1bd375b2222e31
escape0707 <tothesong@gmail.com>
2020-07-29 14:58:13 +0800

    Try to fix verify.ps1

--- verify.ps1
+++ verify.ps1
@@ -1,13 +1,13 @@
 # This script is currently not working.
-# Besides that, for capatibility with Windows 7, please use verify.cmd
+# Besides that, for compatibility with Windows 7, please use verify.cmd
 $problem = "383077-E"
-g++ gen.cpp -o gen.exe
-g++ ac.cpp -o ac.exe
-g++ $problem.cpp -o $problem.exe
+g++ rand.cpp -o rand.exe
+g++ std.cpp -o std.exe
+g++ "$problem.cpp" -o "$problem.exe"
 while ($true) {
-  gen.exe
-  cat input.txt | ac.exe > answer.txt
-  cat input.txt | $problem.exe > output.txt
+  .\rand.exe
+  cat input.txt | .\std.exe > answer.txt
+  cat input.txt | ".\$($problem).exe" > output.txt
   if (diff (cat file1) (cat file2)) {
     break
   }
